a4263e0f06d84afcd248f02132f1af9c897aa6e6
tinatinkokoshvili <tinatin@sas.upenn.edu>
2023-11-29 23:26:46 -0500

    index

--- src/LoxStepper.hs
+++ src/LoxStepper.hs
@@ -2,50 +2,48 @@ module LoxStepper where
 
 import Control.Applicative
 import Data.Char qualified as Char
-import LoxSyntax
-import LoxParser
-import ParserLib (Parser)
-import ParserLib qualified as P
-import Test.HUnit (Assertion, Counts, Test (..), assert, runTestTT, (~:), (~?=))
-import Test.QuickCheck qualified as QC
-import State (State)
-import State qualified as S
 import Data.List qualified as List
 import Data.Map (Map, (!?))
 import Data.Map qualified as Map
 import Data.Maybe (fromMaybe, isJust)
-
+import LoxParser
+import LoxSyntax
+import ParserLib (Parser)
+import ParserLib qualified as P
+import State (State)
+import State qualified as S
+import Test.HUnit (Assertion, Counts, Test (..), assert, runTestTT, (~:), (~?=))
+import Test.QuickCheck qualified as QC
 
 type Environment = Map Name Table
 
 type Table = Map Value Value
 
-data Store = St { environment :: Environment, parent :: Maybe Environment}
+data Store = St {environment :: Environment, parent :: Maybe Environment}
 
-globalTableName  :: Name
+globalTableName :: Name
 globalTableName = "_G"
 
 initialStore :: Store
-initialStore = St { environment = Map.singleton globalTableName Map.empty, parent = Nothing}
+initialStore = St {environment = Map.singleton globalTableName Map.empty, parent = Nothing}
 
 extendedStore :: Store
 extendedStore =
-    St
-        {
-        environment = Map.fromList
-            [ ( globalTableName,
-                Map.fromList
-                [ (StringVal "x", IntVal 3)]
+  St
+    { environment =
+        Map.fromList
+          [ ( globalTableName,
+              Map.fromList
+                [(StringVal "x", IntVal 3)]
             ),
             ( "_t1",
-                Map.fromList
+              Map.fromList
                 [ (StringVal "y", BoolVal True)
                 ]
             )
-            ],
-        parent = Nothing
-        }
-
+          ],
+      parent = Nothing
+    }
 
 type Reference = (Name, Value)
 
@@ -61,10 +59,27 @@ index (t, n) = do
   let valMaybe = let env = environment store in env !? t >>= (!? n)
   return $ fromMaybe NilVal valMaybe
 
+index2 :: Reference -> State Store Value
+index2 reference = do
+  store <- S.get
+  let valMaybe = lookupReference (environment store) reference
+  case valMaybe of
+    Just val -> return val
+    Nothing -> case parent store of
+      Just parentEnv -> do
+        S.put store {environment = parentEnv}
+        index2 reference
+      Nothing -> return NilVal
+
+lookupReference :: Environment -> Reference -> Maybe Value
+lookupReference env (t, n) = do
+  table <- env !? t
+  table !? n
+
 test_index :: Test
 test_index =
-  "index tests" ~:
-    TestList
+  "index tests"
+    ~: TestList
       [ -- The global variable "x" is unitialized (i.e. not present in the initial store)
         S.evalState (index xref) initialStore ~?= NilVal,
         -- But there is a value for "x" available in the extended store
@@ -74,9 +89,8 @@ test_index =
         -- We should also be able to access "t[1]" in the extended store
         S.evalState (index yref) extendedStore ~?= BoolVal True
         -- Updates using the `nil` key are ignored
-        --S.execState (update ("_t1", NilVal) (IntVal 3)) extendedStore ~?= extendedStore
+        -- S.execState (update ("_t1", NilVal) (IntVal 3)) extendedStore ~?= extendedStore
       ]
 
 -- >>> runTestTT test_index
 -- Counts {cases = 4, tried = 4, errors = 0, failures = 0}
-
