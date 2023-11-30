52831df71e9e13d6c6bc0f224ea54092d229ca04
andy jiang <andyjiang55@yahoo.com>
2023-11-29 21:33:40 -0500

    changes

--- test/ParserTests.hs
+++ test/ParserTests.hs
@@ -1,11 +1,44 @@
 module ParserTests where
 
+import Control.Applicative
 import LoxParser
 import LoxSyntax
 import ParserLib qualified as P
+import Test.HUnit (Assertion, Counts, Test (..), assert, runTestTT, (~:), (~?=))
 import Test.QuickCheck as QC
 import Text.PrettyPrint (Doc, (<+>))
 import Text.PrettyPrint qualified as PP
+
+test_wsP :: Test
+test_wsP =
+  TestList
+    [ P.parse (wsP P.alpha) "a" ~?= Right 'a',
+      P.parse (many (wsP P.alpha)) "a b \n   \t c" ~?= Right "abc"
+    ]
+
+test_stringP :: Test
+test_stringP =
+  TestList
+    [ P.parse (stringP "a") "a" ~?= Right (),
+      P.parse (stringP "a") "b" ~?= Left "No parses",
+      P.parse (many (stringP "a")) "a  a" ~?= Right [(), ()]
+    ]
+
+test_constP :: Test
+test_constP =
+  TestList
+    [ P.parse (constP "&" 'a') "&  " ~?= Right 'a',
+      P.parse (many (constP "&" 'a')) "&   &" ~?= Right "aa"
+    ]
+
+test_stringValP :: Test
+test_stringValP =
+  TestList
+    [ P.parse stringValP "\"a\"" ~?= Right (StringVal "a"),
+      P.parse stringValP "\"a\\\"\"" ~?= Right (StringVal "a\\"),
+      P.parse (many stringValP) "\"a\"   \"b\"" ~?= Right [StringVal "a", StringVal "b"],
+      P.parse (many stringValP) "\" a\"   \"b\"" ~?= Right [StringVal " a", StringVal "b"]
+    ]
 
 -- pretty :: (BP.PP a) => a -> String
 -- pretty = PP.render . BP.pp
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,6 @@
-import Lib
 import Test.HUnit (Assertion, Test (TestList), runTestTT, (~:), (~?=))
 import Test.QuickCheck
 
 main :: IO ()
 main = do
-  putStrLn someFunc
   putStrLn "Test suite not yet implemented"
